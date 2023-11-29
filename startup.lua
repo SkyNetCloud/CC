7781d6dccc2ce572d3d38e7f364bac9c0a889e0a
SkyNetCloud <mcboxer1201@gmail.com>
2023-11-29 23:51:03 +0000

    update startup lua

--- startup.lua
+++ startup.lua
@@ -2,3 +2,16 @@ local m = peripheral.wrap("right")
 local rsBridge = peripheral.find("rsBridge")
 
 
+function screen()
+    local size = m.getSize()
+    if size < 5 then 
+     print("Size is less then 5 blocks")
+    end
+end
+
+screen()
+
+while true do
+   print("Log:")
+   sleep(1)
+end

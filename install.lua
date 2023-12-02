d1a89387288b1e4530e1e9941c4446fba44ac161
SkyNetCloud <mcboxer1201@gmail.com>
2023-12-02 01:16:20 +0000

    Updated Installer

--- install.lua
+++ install.lua
@@ -1,70 +1,151 @@
-
 bootstrap = "WdiT6sR5"
 
 installer = "SkyNetCloud/CC/master/installer.lua"
 
-meBridge_display = "SkyNetCloud/CC/master/meBridge_display.lua"
+meBridge_display_github = "SkyNetCloud/CC/master/meBridge_display.lua"
 
-rsBridge_display = "SkyNetCloud/CC/master/rsBridge_display.lua"
+rsBridge_display_github = "SkyNetCloud/CC/master/rsBridge_display.lua"
 
 local meBridge
-local rsBridge 
+local rsBridge
 term.clear()
 
 
-function display_text_term(x,y,text,text_color,bg_color)
+function display_text_term(x, y, text, text_color, bg_color)
     term.setTextColor(text_color)
     term.setBackgroundColor(bg_color)
-    term.setCursorPos(x,y)
+    term.setCursorPos(x, y)
     write(text)
 end
 
-function display_line_text(x,y,length,color)
+function display_line_text(x, y, length, color)
     term.setBackgroundColor(color)
-    term.setCursorPos(x,y)
-    term.write(string.rep("",length))   
+    term.setCursorPos(x, y)
+    term.write(string.rep("", length))
 end
 
-
-
-function progress_bar_term(x,y,length,minVal,maxVal,bar_color,bg_color)
-    display_line_text(x,y,length,bg_color)
-    local barSize = math.floor((minVal/maxVal)* length)
-    display_text_term(x,y,barSize,bar_color)
+function progress_bar_term(x, y, length, minVal, maxVal, bar_color, bg_color)
+    display_line_text(x, y, length, bg_color)
+    local barSize = math.floor((minVal / maxVal) * length)
+    display_text_term(x, y, barSize, bar_color)
 end
 
 function menu_bars()
-   display_text_term(1,1,55,colors.blue)
-   draw_text_term(10,1, "Storage Info Display Info Installer", colors.white,colors.blue)
+    display_text_term(1, 1, 55, colors.blue)
+    draw_text_term(10, 1, "Storage Info Display Info Installer", colors.white, colors.blue)
 
-   draw_line_term(1,18,55,colors.blue)
-   draw_line_term(1,19,55,colors.blue)
-   draw_text_term(10, 18, "by SkyNetCloud", colors.white, colors.blue)
+    draw_line_term(1, 18, 55, colors.blue)
+    draw_line_term(1, 19, 55, colors.blue)
+    draw_text_term(10, 18, "by SkyNetCloud", colors.white, colors.blue)
 end
 
-
-
-function install(program,github)
-
+function install(program, github)
     term.clear()
     menu_bars()
 
 
-    display_text_term(1,3,"Installing "..program.."....", colors.yellow,colors.black)
-    term.setCursorPos(1,5)
+    display_text_term(1, 3, "Installing " .. program .. "....", colors.yellow, colors.black)
+    term.setCursorPos(1, 5)
     term.setTextColor(colors.white)
     sleep(0.5)
 
-    if fs.exists("github") then 
-      display_line_text(1,4, "Github has been isntalled skipping this step")
-    else 
-      install_github()
+    if fs.exists("github") then
+        display_line_text(1, 4, "Github has been isntalled skipping this step")
+    else
+        install_github()
     end
+
+    if fs.exists(program.."_old") then
+      fs.delete(program.."_old")       
+    end
+
+    if fs.exists("config.txt") then
+     fs.delete("config.txt")
+    end
+    if fs.exists(program) then
+        fs.copy(program, program.."_old")
+        fs.delete(program)
+    end
+
+    shell.run("github get"..github.." "..program)
     
 end
 
-function install_github(program,pastebin)
-    shell.run("pastebin get"..pastebin..program)
+function install_github(program, pastebin)
+    shell.run("pastebin get" .. pastebin .. program)
     sleep(0.5)
     shell.run("bootstrap")
+end
+
+function meBridgeSearch()
+    local names = peripheral.getNames()
+    local i, name
+    for i, name in pairs(names) do
+        if peripheral.getType(name) == "meBridge" then
+            return peripheral.wrap(name)
+        else
+
+        end
+    end
+end
+
+function rsBridgeSearch()
+    local names = peripheral.getNames()
+    local i, name
+    for i, name in pairs(names) do
+        if peripheral.getType(name) == "rsBridge" then
+            return peripheral.wrap(name)
+        else
+
+        end
+    end
+end
+
+function selectProgram()
+    term.clear()
+    menu_bars()
+    display_text_term(1, 4, "What would like to update/install? ", colors.yellow, colors.black)
+    display_text_term(3, 6, "1 - RS Info Display", colors.white, colors.black)
+    display_text_term(3, 7, "2 - AE Info Display", colors.white, colors.black)
+    display_text_term(1, 9, "Enter 1 or 2:", colors.yellow, colors.black)
+
+    term.setCursorPos(1, 10)
+    term.setTextColor(colors.white)
+    input = read()
+
+    if input == "1" then
+        install("rsBridge_display", rsBridge_display_github)
+    else
+        if input == "2" then
+            install("meBridge_display", meBridge_display_github)
+        else
+            display_text_term(1, 12, "Please enter a '1' or '2'.", colors.red, colors.black)
+            sleep(1)
+            start()
+        end
+    end
+
+
+    function start()
+        term.clear()
+        menu_bars()
+
+        if fs.exists("config.txt") then
+            if fs.exists("meBridge_display") then
+                display_text_term(2, 3, "Current Program:", colors.white, colors.black)
+                display_text_term(2, 4, "Storage Display", colors.lime, colors.black)
+                display_text_term(1, 6, "Do you want to update this program? (y/n)", colors.white, colors.black)
+                display_text_term(1, 7, "This will delete the current program and any saved settings", colors.gray,
+                    colors.black)
+                term.setCursorPos(1, 9)
+                term.setTextColor(colors.white)
+                input = read()
+                if input == "y" then
+                    install("rsBridge_display", rsBridge_display_github)
+                elseif input == "n" then
+                    selectProgram()
+                end
+            end
+        end
+    end
 end
